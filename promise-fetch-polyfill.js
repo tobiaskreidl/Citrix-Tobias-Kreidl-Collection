6886f5a48045b0f3a02f1b1588c72f8029508f64
tobiaskreidl <tjkreidl@gmail.com>
2026-05-19 15:18:53 -0400

    Add files via upload

---
+++ promise-fetch-polyfill.js
@@ -0,0 +1,27 @@
+var _____WB$wombat$assign$function_____=function(name){return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name))||self[name];};if(!self.__WB_pmw){self.__WB_pmw=function(obj){this.__WB_source=obj;return this;}}{
+let window = _____WB$wombat$assign$function_____("window");
+let self = _____WB$wombat$assign$function_____("self");
+let document = _____WB$wombat$assign$function_____("document");
+let location = _____WB$wombat$assign$function_____("location");
+let top = _____WB$wombat$assign$function_____("top");
+let parent = _____WB$wombat$assign$function_____("parent");
+let frames = _____WB$wombat$assign$function_____("frames");
+let opens = _____WB$wombat$assign$function_____("opens");
+"undefined"==typeof Promise&&document.write('<script src="https://web.archive.org/web/20220528010215/https://d2x5ku95bkycr3.cloudfront.net/HigherLogic/JavaScript/bluebird.3.5.0.min.js"><\/script>'),"undefined"==typeof fetch&&document.write('<script src="https://web.archive.org/web/20220528010215/https://d2x5ku95bkycr3.cloudfront.net/HigherLogic/JavaScript/fetch-polyfill.js"><\/script>');
+}
+
+/*
+     FILE ARCHIVED ON 01:02:15 May 28, 2022 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 14:56:57 May 16, 2026.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  capture_cache.get: 0.447
+  load_resource: 88.826 (2)
+  PetaboxLoader3.datanode: 64.011 (2)
+  PetaboxLoader3.resolve: 23.677
+*/
